b2e55add933fc486734b3c4efcb2045ce24335bd
Raushan1234567 <raushankumar241099@gmail.com>
2023-12-07 08:47:58 +0530

    Print the message of greeting

---
+++ react5/hello/src/App.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+import './App.css';
+import Greet from './Event';
+
+function App() {
+  return (
+    <div className="App">
+      <Greet name="Raushan"/>
+      <Greet name="Amit"/>
+      <Greet name="Sanjay"/>
+      <h1>What are you doing</h1>
+    </div>
+  );
+}
+
+export default App;
